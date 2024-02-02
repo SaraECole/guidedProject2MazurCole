f37c19c6c1fd7dfbee04ab0f5b85f4a70c16866f
SaraECole <cole.sara.e@gmail.com>
2024-02-02 17:36:05 -0500

    added react app with routes

--- server/server.js
+++ server/server.js
@@ -1,8 +1,11 @@
+const cors = require("cors");
 const express = require("express");
 const dao = require("./data_access");
 const bodyParser = require("body-parser");
 const app = express();
+
 app.use(bodyParser.json()); //Parse JSON body
+app.use(cors());
 
 app.get("/api/planets", function (req, res) {
   dao.call("findAllPlanets", {}, (result) => {
